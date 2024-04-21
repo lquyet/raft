5313300edfc0f5b886a3adfe3adce545418a3b85
lquyet <anhquyethgvx@gmail.com>
2024-04-21 13:56:59 +0700

    finish cluster demo

--- cluster.go
+++ cluster.go
@@ -1,20 +1,51 @@
 package distributed_lock
 
+import "fmt"
+
 type RaftCluster struct {
 	Cluster      []*Server
 	NumOfServers int
+	Connected    []bool
 }
 
-func NewRaftCluster(n int) {
+func NewRaftCluster(n int) *RaftCluster {
 	ns := make([]*Server, n)
 	ready := make(chan interface{})
+	connected := make([]bool, n)
 
 	for i := 0; i < n; i++ {
-		peerIds := make([]int, 0)
+		peerIds := make([]int32, 0)
+		peerAddrs := make(map[int32]string)
 		for p := 0; p < n; p++ {
 			if p != i {
-				peerIds = append(peerIds, p)
+				peerIds = append(peerIds, int32(p))
+				peerAddrs[int32(p)] = fmt.Sprintf("localhost:%d", 8080+p)
 			}
 		}
+		fmt.Println(peerIds, peerAddrs)
+		ns[i] = NewServer(int32(i), peerIds, peerAddrs, ready, "localhost:808"+fmt.Sprintf("%d", i))
+		go func(i int) {
+			ns[i].Serve()
+		}(i)
 	}
+
+	// Connect all peers to each other.
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if i != j {
+				ns[i].ConnectToPeer(int32(j))
+			}
+		}
+		connected[i] = true
+	}
+
+	close(ready)
+
+	raftCluster := RaftCluster{
+		Cluster:      ns,
+		NumOfServers: n,
+		Connected:    connected,
+	}
+
+	return &raftCluster
 }
--- main/main.go
+++ main/main.go
@@ -3,7 +3,7 @@ package main
 import distributed_lock "github.com/lquyet/distributed-lock"
 
 func main() {
-	ready := make(chan interface{})
-	server := distributed_lock.NewServer(1, []int32{}, map[int32]string{}, ready, "localhost:8080")
-	server.Serve()
+	_ = distributed_lock.NewRaftCluster(3)
+	c := make(chan struct{})
+	<-c
 }
