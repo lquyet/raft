68becfc75c128e108fef1f7deebdae900a780ed6
lquyet <anhquyethgvx@gmail.com>
2024-04-21 12:45:23 +0700

    test single instance, implement cluster

--- main/main.go
+++ main/main.go
@@ -1,8 +1,9 @@
 package main
 
-import "github.com/lquyet/distributed-lock"
+import distributed_lock "github.com/lquyet/distributed-lock"
 
 func main() {
-	server := distributed_lock.NewServer(1, []int32{}, map[int32]string{}, make(chan interface{}), "localhost:8080")
+	ready := make(chan interface{})
+	server := distributed_lock.NewServer(1, []int32{}, map[int32]string{}, ready, "localhost:8080")
 	server.Serve()
 }
